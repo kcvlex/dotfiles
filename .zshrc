8f5c1af8ca664c3116445684f04d5a3f31bcd85a
kcvlex <krkr9893@gmail.com>
2024-05-24 10:51:54 +0900

    init.lua

--- .zshrc
+++ .zshrc
@@ -37,7 +37,7 @@ export JAVA_HOME=/usr/lib/jvm/default
 export PATH="${HOME}/.local/bin:${PATH}"
 export PATH="${PATH}:${HOME}/go/bin"
 
-eval $(opam env)
+# eval $(opam env)
 
 PROMPT="%K{000}%F{040}[%n@%m %1~]%f%k "
 
@@ -49,12 +49,12 @@ compinit
 #source /usr/share/git/completion/git-completion.zsh
 #PS1='[%n@%m %c$(__git_ps1 " (%s)")]\$ '
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/kcvlex/gomi/google-cloud-sdk/path.zsh.inc' ]; then . '/home/kcvlex/gomi/google-cloud-sdk/path.zsh.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/home/kcvlex/gomi/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/kcvlex/gomi/google-cloud-sdk/completion.zsh.inc'; fi
-
 export GOPROXY=direct
 
-powerline-daemon -q
+# powerline-daemon -q
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/kcvlex/google-cloud-sdk/path.zsh.inc' ]; then . '/home/kcvlex/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/kcvlex/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/kcvlex/google-cloud-sdk/completion.zsh.inc'; fi
--- init.lua
+++ init.lua
@@ -1,17 +1,23 @@
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
-	Plug 'tpope/vim-fugitive'
-	Plug 'AlessandroYorba/Alduin'
-	Plug 'miyakogi/seiya.vim'
-	Plug 'simeji/winresizer'
-	Plug 'rust-lang/rust.vim'
-	Plug 'vim-airline/vim-airline'
-	Plug 'vim-airline/vim-airline-themes'
-	Plug 'prabirshrestha/vim-lsp'
-	Plug 'mattn/vim-lsp-settings'
-	Plug 'neovim/nvim-lspconfig'
+    Plug 'tpope/vim-fugitive'
+    Plug 'AlessandroYorba/Alduin'
+    Plug 'miyakogi/seiya.vim'
+    Plug 'simeji/winresizer'
+    Plug 'rust-lang/rust.vim'
+    Plug 'vim-airline/vim-airline'
+    Plug 'vim-airline/vim-airline-themes'
+    Plug 'prabirshrestha/vim-lsp'
+    Plug 'mattn/vim-lsp-settings'
+    Plug 'neovim/nvim-lspconfig'
     Plug 'Yggdroot/indentLine'
+    Plug 'hashivim/vim-terraform'
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/cmp-path'
+    Plug 'hrsh7th/cmp-cmdline'
+    Plug 'hrsh7th/nvim-cmp'
 vim.call('plug#end')
 
 vim.g.indent_guides_start_level = 2
@@ -49,7 +55,9 @@ local on_lsp_buffer_enabled = function()
     end
 
     vim.keymap.set('n', 'gd', '<plug>(lsp-definition)', { buffer = true })
-    vim.keymap.set('n', 'gs', '<plug>(lsp-document-symbol-search)', { buffer = true })
+    -- vim.keymap.set('n', 'gs', '<plug>(lsp-document-symbol-search)', { buffer = true })
+    vim.keymap.set('n', 'gs', '<C-w>]', { buffer = true })
+    vim.keymap.set('n', 'gv', '<C-w>v', { buffer = true })
     vim.keymap.set('n', 'gS', '<plug>(lsp-workspace-symbol-search)', { buffer = true })
     vim.keymap.set('n', 'gr', '<plug>(lsp-references)', { buffer = true })
     vim.keymap.set('n', 'gi', '<plug>(lsp-implementation)', { buffer = true })
@@ -59,6 +67,40 @@ local on_lsp_buffer_enabled = function()
     -- nmap <buffer> ]g <plug>(lsp-next-diagnostic)
     -- nmap <buffer> K <plug>(lsp-hover)
 end
+
+local cmp = require'cmp'
+cmp.setup({
+snippet = {
+  -- REQUIRED - you must specify a snippet engine
+  expand = function(args)
+    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+  end,
+},
+window = {
+  -- completion = cmp.config.window.bordered(),
+  -- documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+  { name = 'nvim_lsp' },
+  { name = 'vsnip' }, -- For vsnip users.
+  -- { name = 'luasnip' }, -- For luasnip users.
+  -- { name = 'ultisnips' }, -- For ultisnips users.
+  -- { name = 'snippy' }, -- For snippy users.
+}, {
+  { name = 'buffer' },
+})
+})
 
 local lsp_group = vim.api.nvim_create_augroup('lsp_install', { clear = true })
 vim.api.nvim_create_autocmd({ 'User' }, {
